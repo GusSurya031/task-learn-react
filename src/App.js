70bdf81207983679d83f7f40cbdc7482386af996
Gus_Surya031 <suryadiyatmika31@gmail.com>
2023-07-17 14:50:02 +0800

    first task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/cardProduct';
+import React,{ useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Welcom to the first learn!
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      <div className='background'>
+        <h1>LIST PRODUK</h1>
+        <div className='container_card'>
+          <Card
+            name="Double Oreo Mile Crepes"
+            price={30.15}
+            image= {process.env.PUBLIC_URL + '/oreo-mile-crepes.jpg'}
+            description={"lorem ipsum sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"}
+            // className="Card"
+          />
+          <Card
+            name="Choco Mile Crepes"
+            price={54.20}
+            image= {process.env.PUBLIC_URL + '/double-choco.jpg'}
+            description={"lorem ipsum sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"}
+            // className="Card"
+          />
+          <Card
+            name="Strawberry Mile Crepes"
+            price={40.05}
+            image= {process.env.PUBLIC_URL + '/strawberry-mile-crepes.jpg'}
+            description={"lorem ipsum sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"}
+            // className="Card"
+          />
+          
+        </div>
+      </div>
+    
+    
+    
+    </>
+
+
   );
 }
 
